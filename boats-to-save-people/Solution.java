5d3ab124365478ebd8a46bf2b1e4041ab7929707
Nisarg <emailtonisarg@gmail.com>
2020-07-06 15:35:46 -0400

    boats-to-save-people

---
+++ boats-to-save-people/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int boats = 0;
+        int i,j;
+        for(i =0, j = people.length - 1; i <= j; j--){
+            if(people[i] + people[j] <= limit){
+                i++;
+            }
+        }
+        return people.length - 1 - j;
+    }
+}
